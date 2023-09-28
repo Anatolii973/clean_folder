61a5accfba6bd8618105c5b7b48af63999e09a98
Anatolii973 <tolkedis73@gmail.com>
2023-09-28 21:04:44 +0300

    28,09

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
 
-setup(name="clean",
-      version="0.1.0"
+setup(name="clean_folder",
+      version="0.1.0",
+      description="My class",
+      url="https://github.com/LAV1970/Homework_2",
+      author="Anatoliy Kedis",
+      author_email="tolkedis73@gmail.com",
+      license="MIT",
+      packages=["clean_folder"],
+      entry_points={"console_scripts": ["clean-folder = clean_folder.clean:main"]},
       )
+      
